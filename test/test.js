cbf531a98183169543b530e7509208e4c511670d
Stephen Rivas JR <stephen@zeunic.com>
2013-04-05 02:22:07 -0400

    Menu bar tinkerings/bootstrap while I think of IA and design. Removed uncessary node engine tests.

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
 var expect = require('expect.js');
 
 describe('Starter Tests', function(){
-	it('Should start with tests', function(){
+	it('Should start with tests', function(done){
 		expect(true).to.be.ok();
+		done();
 	});
 });
