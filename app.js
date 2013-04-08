acfed855488b893389a95d01a7a036b4004088ea
Stephen Rivas JR <stephen@zeunic.com>
2013-04-08 19:15:04 -0400

    Bootstrap theme, Resume File, Routes & Views for simple pages

--- app.js
+++ app.js
@@ -4,32 +4,37 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path');
+	, routes = require('./routes')
+	, http = require('http')
+	, path = require('path');
 
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.cookieParser('your secret here'));
+	app.use(express.session());
+	app.use(app.router);
+	app.use(require('stylus').middleware(__dirname + '/public'));
+	app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 });
 
+app.get('/about', routes.about);
+app.get('/blog', routes.blog);
+app.get('/contact', routes.contact);
 app.get('/', routes.index);
+app.get('/resume', routes.resume);
+app.get('/work', routes.work);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+	console.log("Express server listening on port " + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,32 @@
 
 /*
- * GET home page.
+ * Static / Simple Routes
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Progress!' });
+var titlePrefix = 'Stephen Rivas JR | ';
+
+
+exports.about = function(req, res) {
+	res.render('about', { title: titlePrefix + 'About' });
+};
+
+// this will move eventually to its own class for dynamic content
+exports.blog = function(req, res) {
+	res.render('blog', { title: titlePrefix + 'Blog' });
+};
+
+exports.contact = function(req, res) {
+	res.render('contact', { title: titlePrefix + 'Contact' });
+};
+
+exports.index = function(req, res) {
+	res.render('index', { title: titlePrefix + 'Home' });
+};
+
+exports.resume = function(req, res) {
+	res.render('resume', { title: titlePrefix + 'Résumé' });
+};
+
+exports.work = function(req, res) {
+	res.render('resume', { title: titlePrefix + 'Work' });
 };
