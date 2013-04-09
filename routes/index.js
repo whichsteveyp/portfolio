d85ccd41350c49f11167fd0c724084750e70870a
Stephen Rivas JR <stephen@zeunic.com>
2013-04-09 01:35:42 -0400

    Added .active class to menu for routes

--- routes/index.js
+++ routes/index.js
@@ -3,30 +3,29 @@
  * Static / Simple Routes
  */
 
-var titlePrefix = 'Stephen Rivas JR | ';
-
+// var titlePrefix = 'Stephen Rivas JR | ';
 
 exports.about = function(req, res) {
-	res.render('about', { title: titlePrefix + 'About' });
+	res.render('about', { title: 'About', active: '/about' });
 };
 
 // this will move eventually to its own class for dynamic content
 exports.blog = function(req, res) {
-	res.render('blog', { title: titlePrefix + 'Blog' });
+	res.render('blog', { title: 'Blog', active: '/blog' });
 };
 
 exports.contact = function(req, res) {
-	res.render('contact', { title: titlePrefix + 'Contact' });
+	res.render('contact', { title: 'Contact', active: '/contact' });
 };
 
 exports.index = function(req, res) {
-	res.render('index', { title: titlePrefix + 'Home' });
+	res.render('index', { title: 'Home', active: '/' });
 };
 
 exports.resume = function(req, res) {
-	res.render('resume', { title: titlePrefix + 'Résumé' });
+	res.render('resume', { title: 'Résumé', active: '/resume' });
 };
 
 exports.work = function(req, res) {
-	res.render('resume', { title: titlePrefix + 'Work' });
+	res.render('resume', { title: 'Work', active: '/work' });
 };
